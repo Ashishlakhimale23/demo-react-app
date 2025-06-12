a546d25d3cb9fc689863252a3b2faed1b9ea9331
AshishLakhimale23 <ashishlakhimale23@gmail.com>
2025-06-12 15:53:28 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const url = process.env.REDIS_URL
 
   return (
     <>
@@ -22,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {url}
         </p>
       </div>
       <p className="read-the-docs">
