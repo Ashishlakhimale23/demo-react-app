b6a1d98666f678d6a4078a950d52a03b510d97c8
Ashishlakhimale23 <124334273+Ashishlakhimale23@users.noreply.github.com>
2025-06-15 00:45:19 +0530

    no change in the ui

--- src/App.tsx
+++ src/App.tsx
@@ -21,14 +21,11 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} helllo world
-        </button>
-         
+          count is {count} 
+        </button>  
         
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
