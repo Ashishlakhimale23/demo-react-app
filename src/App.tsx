7d6bcfc042cf662d3d7cf313b240801e9c5a6473
Ashishlakhimale23 <124334273+Ashishlakhimale23@users.noreply.github.com>
2025-06-14 23:56:51 +0530

    working

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} hello
+          count is {count}
         </button>
          
         
