1be92a59172a69323b54a87c17d4fa7b13bcf202
Ashishlakhimale23 <124334273+Ashishlakhimale23@users.noreply.github.com>
2025-06-15 16:41:46 +0530

    verifying the status is been update or not

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} verifying the status is been update or not
         </button>  
         
       </div>
